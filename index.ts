a48acce8ab4918b1fa54c9b3f7770d2cc0a19f3a
chafyaay <y.chafyaay@gmail.com>
2020-10-16 14:23:42 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,32 @@
 // Import stylesheets
 import './style.css';
 
+const gen=generator();
+gen.next();
+gen.next();
+let  h=hello()
+
+
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1>TypeScript Starter 
+${h}
+
+
+
+</h1>`;
+
+//generator
+
+function *generator(){
+  yield 1;
+  hello("1")
+   yield 2;
+  hello("2")
+   yield 3;
+  hello("3")
+}
+
+function hello(id?){
+  return "hello"+id;
+}
